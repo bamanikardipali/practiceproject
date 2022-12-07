fd2b2737ec2fd06e4b886c4acb90e58abb3b28a1
Arnav <Arnav@DESKTOP-PCMCSF2>
2022-12-07 17:34:35 +0530

    [dipali]:push code

---
+++ src/Demo/Sample2.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class Sample2
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi good evening");
+		System.out.println("Dipali Yuvraj Bamaniakr");
+	}
+}
